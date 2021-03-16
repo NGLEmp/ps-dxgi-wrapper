6cdff69f8576f34d328f1c58ab9b3f2625f40533
NGLEmp <49843311+NGLEmp@users.noreply.github.com>
2021-03-16 10:46:08 +0000

    Added destination folder to copy commands
    Added missing `"%dest%"` parameter.

--- install.cmd
+++ install.cmd
@@ -11,8 +11,8 @@ copy /b gpu.ini "%dest%"
 :: REQUIRED
 :: VC Runtime Proxy
 :VCRuntimeProxy
-copy /b vcruntime140_1.dll
-copy /b vcruntime_ldr.ini
+copy /b vcruntime140_1.dll "%dest%"
+copy /b vcruntime_ldr.ini "%dest%"
 
 :: Install only required components
 goto End
@@ -22,13 +22,13 @@ rem goto d3d12onV
 :: NOT TESTED
 :: Wine, Windows 7 and 8.x support (videocard info only)
 :d3d12on7
-copy /b d3d12on7.dll
-copy /b dxilconv7.dll
+copy /b d3d12on7.dll "%dest%"
+copy /b dxilconv7.dll "%dest%"
 rem goto End
 
 :: OPTIONAL
 :: Vulkan Support (videocard info only)
 :d3d12onV
-copy /b d3d12onV.dll
+copy /b d3d12onV.dll "%dest%"
 
 :End
